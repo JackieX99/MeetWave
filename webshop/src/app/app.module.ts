c8a78de35c332ece1ad250307bf4cb9a69adafde
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-04-20 20:02:04 +0200

    Teszt npm csomagok

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TestComponent } from './_components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
