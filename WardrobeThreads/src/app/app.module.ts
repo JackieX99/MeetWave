063bd700e89bb0729522fe854afce5b5ae817351
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-04-26 23:02:40 +0200

    Squashed commit of the following:
    commit 8b1e358e730ca94eb37eae71a0f53d566fde6e52
    Author: Földvári Alex <43179105+JackieX99@users.noreply.github.com>
    Date:   Wed Apr 26 22:14:53 2023 +0200
    
        Environment setup, firebase npm setup
    
    commit dad0b14a13b9e10fff31641e86d55a2595cc9a31
    Author: Földvári Alex <43179105+JackieX99@users.noreply.github.com>
    Date:   Wed Apr 26 18:06:57 2023 +0200
    
        Alap komponensek

--- WardrobeThreads/src/app/app.module.ts
+++ WardrobeThreads/src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { RegisterComponent } from './_components/register/register.component';
+import { LoginComponent } from './_components/login/login.component';
+import { ShopCartComponent } from './_components/shop-cart/shop-cart.component';
+import { ProfileInformationsComponent } from './_components/profile-informations/profile-informations.component';
+import { PurchaseCompletingComponent } from './_components/purchase-completing/purchase-completing.component';
+import { ProductsComponent } from './_components/products/products.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    ShopCartComponent,
+    ProfileInformationsComponent,
+    PurchaseCompletingComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
