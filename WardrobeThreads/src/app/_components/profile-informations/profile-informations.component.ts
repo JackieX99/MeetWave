dad0b14a13b9e10fff31641e86d55a2595cc9a31
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-04-26 18:06:57 +0200

    Alap komponensek

---
+++ WardrobeThreads/src/app/_components/profile-informations/profile-informations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-informations',
+  templateUrl: './profile-informations.component.html',
+  styleUrls: ['./profile-informations.component.scss']
+})
+export class ProfileInformationsComponent {
+
+}
--- WardrobeThreads/src/app/app.module.ts
+++ WardrobeThreads/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { RegisterComponent } from './_components/register/register.component';
+import { LoginComponent } from './_components/login/login.component';
+import { ShopCartComponent } from './_components/shop-cart/shop-cart.component';
+import { ProfileInformationsComponent } from './_components/profile-informations/profile-informations.component';
+import { PurchaseCompletingComponent } from './_components/purchase-completing/purchase-completing.component';
+import { ProductsComponent } from './_components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    ShopCartComponent,
+    ProfileInformationsComponent,
+    PurchaseCompletingComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
