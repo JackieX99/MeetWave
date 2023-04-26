89a0ffc948b136430264601bd468e739bd582a50
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-04-26 18:07:27 +0200

    Squashed commit of the following:
    commit dad0b14a13b9e10fff31641e86d55a2595cc9a31
    Author: Földvári Alex <43179105+JackieX99@users.noreply.github.com>
    Date:   Wed Apr 26 18:06:57 2023 +0200
    
        Alap komponensek

---
+++ WardrobeThreads/src/app/_components/purchase-completing/purchase-completing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-completing',
+  templateUrl: './purchase-completing.component.html',
+  styleUrls: ['./purchase-completing.component.scss']
+})
+export class PurchaseCompletingComponent {
+
+}
---
+++ WardrobeThreads/src/app/_components/shop-cart/shop-cart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-cart',
+  templateUrl: './shop-cart.component.html',
+  styleUrls: ['./shop-cart.component.scss']
+})
+export class ShopCartComponent {
+
+}
