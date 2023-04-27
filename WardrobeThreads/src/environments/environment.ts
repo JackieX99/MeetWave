bb4dae00441b583429d36d3cd2cc6a55e2665c2f
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-04-27 15:32:03 +0200

    Squashed commit of the following:
    commit 393885c3447d051a69034d1332e7ea75b4406921
    Author: Földvári Alex <43179105+JackieX99@users.noreply.github.com>
    Date:   Thu Apr 27 15:31:04 2023 +0200
    
        Még több komponens
    
    commit c94e81eaa691019db6ca98e67c0fb3f9da8de0ec
    Author: Földvári Alex <43179105+JackieX99@users.noreply.github.com>
    Date:   Thu Apr 27 15:12:26 2023 +0200
    
        Admin-panel komponens
    
    commit 8b1e358e730ca94eb37eae71a0f53d566fde6e52
    Author: Földvári Alex <43179105+JackieX99@users.noreply.github.com>
    Date:   Wed Apr 26 22:14:53 2023 +0200
    
        Environment setup, firebase npm setup

---
+++ WardrobeThreads/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'wardrobethreads-265c9',
+    appId: '1:281953700315:web:57679b0208f4b48b863ffe',
+    databaseURL: 'https://wardrobethreads-265c9-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'wardrobethreads-265c9.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBmSzUr2_Q9mjcQwpxZNszLVYFj-3vZrYE',
+    authDomain: 'wardrobethreads-265c9.firebaseapp.com',
+    messagingSenderId: '281953700315',
+    measurementId: 'G-NVV5R13HYX',
+  },
+  production: false,
+};
