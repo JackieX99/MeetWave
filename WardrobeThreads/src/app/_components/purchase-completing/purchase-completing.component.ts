fdc341f6c2bc202aabcda21f7944c93d085f799c
biskuppos <biskupszabi123@gmail.com>
2023-05-10 18:15:21 +0200

    Purchase-Completing(kezdetleges)

--- WardrobeThreads/src/app/_components/purchase-completing/purchase-completing.component.ts
+++ WardrobeThreads/src/app/_components/purchase-completing/purchase-completing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-completing',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./purchase-completing.component.scss']
 })
 export class PurchaseCompletingComponent {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
 
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) {}
 }
