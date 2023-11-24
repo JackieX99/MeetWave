04b4d8624441743d37aa3b01426f31027c473cc3
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-11-24 13:17:04 +0100

    Új backend és hozzá egy minta endpoint

---
+++ backend/MeetWave-REST/src/main/java/net/meetwave/meetwaverest/MeetWave_REST.java
@@ -0,0 +1,41 @@
+package net.meetwave.meetwaverest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import spark.Spark;
+
+import static spark.Spark.*;
+
+public final class MeetWave_REST extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getConsoleSender().sendMessage("REST-API ELINDULT");
+        Spark.port(10046);
+        Spark.secure("./letsencrypt.jks", "valamijelszoxdxd1234912", null, null);
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
+    }
+
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage("REST-API LEÁLLT");
+    }
+}
