4f485bb63f676426be37d7e46ee1b7336e22dd95
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-12-24 18:15:52 +0100

    localstorage service, footer navigáció a megadott helyekre, főoldal kód változások

--- frontend/MeetWave/src/app/_components/footer/footer.component.ts
+++ frontend/MeetWave/src/app/_components/footer/footer.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/_services/local-storage.service';
+import { MainComponent } from '../main/main.component';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  activeRouteName: string = '';
+
+  constructor(
+    private router: Router,
+    private localStorage: LocalStorageService,
+    private route: ActivatedRoute,
+    private mainComponent: MainComponent
+  ) { }
+
+  navigateToTarget(target: String, page: String) {
+    this.route.url.subscribe(url => {
+      const routeSegments = url.map(segment => segment.path);
+      this.activeRouteName = routeSegments.join('/');
+    });
+    
+    // A felhasználó már az adott oldalon tartózkodik, scroll eventet kell hívni
+    if(this.activeRouteName == page){
+      if(this.activeRouteName == "welcome"){
+        this.mainComponent.navigateToTarget("gyik");
+      }
+    } else{
+      this.localStorage.setItem("routeTo", target);
+      this.router.navigate(['/' + page]);
+    }
+  }
+
+  
 
 }
