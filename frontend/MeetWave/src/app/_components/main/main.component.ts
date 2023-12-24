ff5cdeb7076755dc6fda91a85afd1f344d74993c
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-12-24 18:15:52 +0100

    localstorage service, footer navigáció a megadott helyekre, főoldal kód változások

--- frontend/MeetWave/src/app/_components/main/main.component.ts
+++ frontend/MeetWave/src/app/_components/main/main.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { LocalStorageService } from 'src/app/_services/local-storage.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit {
+
+  @ViewChild('targetGyik') targetGyikElement!: ElementRef;
+  
   panelOpenState = false;
+
+  constructor(private localStorage: LocalStorageService) {}
+
+  ngAfterViewInit(): void {
+    let routeto: String = this.localStorage.getItem("routeTo");
+    this.localStorage.removeItem("routeTo");
+    this.navigateToTarget(routeto)
+  }
+
+  public navigateToTarget(target: String){
+    switch(target){
+      case "gyik":
+        this.targetGyikElement.nativeElement.scrollIntoView({ behavior: 'smooth' });
+        break;
+      default:
+        // do nothing
+    }
+  }
 }
