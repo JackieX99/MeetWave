cb01ffee5def913fa44c3a0571b80151451fe264
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-12-25 16:39:39 +0100

    ../server/status endpoint fix

--- backend/MeetWave/src/main/java/com/radnoti/meetwave/Controller/ServerController.java
+++ backend/MeetWave/src/main/java/com/radnoti/meetwave/Controller/ServerController.java
@@ -4,10 +4,7 @@ import com.radnoti.meetwave.Service.ServerService;
 import com.radnoti.meetwave.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,13 +20,12 @@ public class ServerController {
         this.server = server;
     }
 
-    @PostMapping("/status")
-    public ResponseEntity<Map<String, Object>> getUser(@RequestBody Map<String, Integer> requestBody) {
-        Map<String, Object> result = server.CheckServerStatus();
-
-        System.out.println(result);
-
-        result.put("status", "success");
+    @GetMapping("/status")
+    public ResponseEntity<Map<String, Object>> getUser() {
+        Map<String, Object> status = server.CheckServerStatus();
+        Map<String, Object> result = new HashMap<>();
+        
+        result.put("status", status.get("status"));
 
         return ResponseEntity.ok(result);
     }
