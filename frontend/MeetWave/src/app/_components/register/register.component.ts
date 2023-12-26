102f7b28713ae013e145c0db7d21038b7accc252
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-12-26 17:59:20 +0100

    regisztrációs oldal mobil nézet

--- frontend/MeetWave/src/app/_components/register/register.component.ts
+++ frontend/MeetWave/src/app/_components/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { StepperOrientation } from '@angular/cdk/stepper';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  profileForm = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    passwordRe: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+  });
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  formClicked() {
+    // const logoElement = this.el.nativeElement.querySelector('.logo');
+    // const formElement = this.el.nativeElement.querySelector('.form');
+  
+    // this.renderer.addClass(logoElement, 'moveLogo');
+    // this.renderer.addClass(formElement, 'formBackground');
+  }
 
 }
