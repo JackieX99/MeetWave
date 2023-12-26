7ab698f3566177b51c9b14b2b6fd55e0ad320354
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-12-26 17:59:20 +0100

    regisztrációs oldal mobil nézet

--- frontend/MeetWave/src/app/app.module.ts
+++ frontend/MeetWave/src/app/app.module.ts
@@ -13,10 +13,20 @@ import { EventComponent } from './_components/event/event.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { MainComponent } from './_components/main/main.component';
 import { HttpClientModule } from  '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { LocalStorageService } from './_services/local-storage.service';
+import {Component} from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {StepperOrientation, MatStepperModule} from '@angular/material/stepper';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {AsyncPipe} from '@angular/common';
+
 
 @NgModule({
   declarations: [
@@ -37,7 +47,14 @@ import { LocalStorageService } from './_services/local-storage.service';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    AsyncPipe,
   ],
   providers: [MainComponent, LocalStorageService],
   bootstrap: [AppComponent]
