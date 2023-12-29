28ba986d8c0d09d1f88204f1806786b125a4d3e6
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2023-12-29 15:07:11 +0100

    Login page, és jelszó input fixek

--- frontend/MeetWave/src/app/_components/login/login.component.ts
+++ frontend/MeetWave/src/app/_components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  profileForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  formClicked() {
+    // const logoElement = this.el.nativeElement.querySelector('.logo');
+    // const formElement = this.el.nativeElement.querySelector('.form');
+  
+    // this.renderer.addClass(logoElement, 'moveLogo');
+    // this.renderer.addClass(formElement, 'formBackground');
+  }
 }
