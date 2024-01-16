947e23502c889fdc85650edfe096b41513729ca8
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2024-01-16 10:05:01 +0100

    Revert "CORS policy javítva"
    This reverts commit fc99766bc6a1c88fbf9ef647121c8818a1d67c99.

--- backend/MeetWave/src/main/java/com/radnoti/meetwave/Config/WebConfig.java
+++ backend/MeetWave/src/main/java/com/radnoti/meetwave/Config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "localhost:4200" , "https://meetwave.hu")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
