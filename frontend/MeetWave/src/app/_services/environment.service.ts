726bea4bc0704e79cb19b496aad70eaefd52a0f7
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2024-01-16 10:25:30 +0100

    env elérés update

--- frontend/MeetWave/src/app/_services/environment.service.ts
+++ frontend/MeetWave/src/app/_services/environment.service.ts
@@ -7,7 +7,8 @@ export class EnvironmentService {
 
   constructor() { }
 
-  url = "https://api.cherrylands.hu";
+  // url = "https://api.cherrylands.hu";
+  url = "http://46.107.215.7:11110";
 
   getVtmUrl(): String {
     return this.url;
