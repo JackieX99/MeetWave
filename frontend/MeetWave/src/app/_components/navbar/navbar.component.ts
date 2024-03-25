f030ce80fae3b138492a92912b6785efaadfa322
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2024-03-25 16:30:09 +0100

    Navbar, NavbarVisibilityService, AppComponent változtatások, új svg ikonok

---
+++ frontend/MeetWave/src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  activeRoute: string | undefined;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.setActiveRoute();
+    });
+  }
+
+  setActiveRoute(): void {
+    this.activeRoute = this.router.url;
+  }
+
+}
